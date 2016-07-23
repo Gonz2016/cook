f3f409eeb45cefb41cbe9ac88da40dc052e9211c
Gonzalo <calderongonz@gmail.com>
2016-07-23 16:23:32 -0700

    Add additional html ul and a JS

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var cookieStand1 = {
   minimumCustomerPerHour: 17,
   maximumCustomerPerHour: 88,
   cookiesPerCustomer: 5.2,
+  cookies: [],
+  totalCookies: 0,
+  addInfo: function() {
+  //setting up a function to get an element from the page by ID    
+  },
   randomCustomerPerHour: function() {
     var customersThisHour = Math.floor( Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
     //console.log(customersThisHour)
@@ -18,18 +23,18 @@ var cookieStand1 = {
     return cookiesThisHour
   },
   cookiesPerDay: function() {
-    var totalCookies = 0
-    var cookies = []
     //calls this cookiesPerHour() 8 times
     for (var i=0; i<8; i++) {
       var cookiesThisHour = this.cookiesPerHour()
-      cookies[i] = cookiesThisHour;
-      totalCookies = totalCookies + cookiesThisHour
-      console.log(totalCookies)
-      console.log(cookies)
+      this.cookies[i] = cookiesThisHour;
+      this.totalCookies = this.totalCookies + cookiesThisHour
+      console.log(this.totalCookies)
+      console.log(this.cookies)
     }
     //stores the results of each call in an array
 
   }
 }
+console.log(cookieStand1)
 cookieStand1.cookiesPerDay()
+console.log(cookieStand1)
