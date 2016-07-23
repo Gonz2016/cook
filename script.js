4deaae737ffb37b1ae04e43fe8f0ff2ac950c313
Gonzalo <calderongonz@gmail.com>
2016-07-23 15:36:42 -0700

    Second commit for this project with an array and loop

--- script.js
+++ script.js
@@ -7,16 +7,29 @@ var cookieStand1 = {
   cookiesPerCustomer: 5.2,
   randomCustomerPerHour: function() {
     var customersThisHour = Math.floor( Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    console.log(customersThisHour)
+    //console.log(customersThisHour)
     return customersThisHour
   },
   cookiesPerHour: function() {
     var customersThisHour = this.randomCustomerPerHour()
     var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
     cookiesThisHour = Math.floor(cookiesThisHour)
-    console.log(cookiesThisHour)
+    //console.log(cookiesThisHour)
     return cookiesThisHour
+  },
+  cookiesPerDay: function() {
+    var totalCookies = 0
+    var cookies = []
+    //calls this cookiesPerHour() 8 times
+    for (var i=0; i<8; i++) {
+      var cookiesThisHour = this.cookiesPerHour()
+      cookies[i] = cookiesThisHour;
+      totalCookies = totalCookies + cookiesThisHour
+      console.log(totalCookies)
+      console.log(cookies)
+    }
+    //stores the results of each call in an array
 
   }
 }
-cookieStand1.cookiesPerHour()
+cookieStand1.cookiesPerDay()
