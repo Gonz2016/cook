efe52ba96f96978202c25ff226da603b52de5854
Gonzalo <calderongonz@gmail.com>
2016-07-30 15:52:35 -0700

    Finally completed project number 6 with a great deal of help for everyone.

--- script.js
+++ script.js
@@ -138,6 +138,96 @@ var WashingtonSquare = {
 }
 
 
+var Sellwood = {
+  name: "Sellwood",
+  minimumCustomerPerHour: 20,
+  maximumCustomerPerHour: 48,
+  cookiesPerCustomer: 3.3,
+  cookies: [],
+  totalCookies: 0,
+  addInfo: function() {
+  //setting up a function to get an element from the page by ID
+    var list = document.getElementById('Sellwood')
+    var info = "<h2>"+this.name+"</h2>"
+    list.innerHTML += info;
+  },
+
+  randomCustomerPerHour: function() {
+    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
+    //console.log(customersThisHour)
+    return customersThisHour
+  },
+  cookiesPerHour: function() {
+    var customersThisHour = this.randomCustomerPerHour()
+    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
+    cookiesThisHour = Math.floor(cookiesThisHour)
+    //console.log(cookiesThisHour)
+    return cookiesThisHour
+  },
+
+  cookiesPerDay: function() {
+    //calls this cookiesPerHour() 8 times
+    for (var i=0; i<8; i++) {
+      var cookiesThisHour = this.cookiesPerHour()
+      this.cookies[i] = cookiesThisHour;
+      this.totalCookies = this.totalCookies + cookiesThisHour
+      var listItem = document.createElement("li");
+      listItem.innerHTML=this.totalCookies;
+
+      var ul = document.getElementById("SellwoodCookies");
+      ul.appendChild(listItem);
+      console.log(this.cookies)
+    }
+    //stores the results of each call in an array
+  }
+}
+
+
+
+var PearlDistrict = {
+  name: "Pearl District",
+  minimumCustomerPerHour: 3,
+  maximumCustomerPerHour: 24,
+  cookiesPerCustomer: 2.6,
+  cookies: [],
+  totalCookies: 0,
+  addInfo: function() {
+  //setting up a function to get an element from the page by ID
+    var list = document.getElementById('PearlDistrict')
+    var info = "<h2>"+this.name+"</h2>"
+    list.innerHTML += info;
+  },
+
+  randomCustomerPerHour: function() {
+    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
+    //console.log(customersThisHour)
+    return customersThisHour
+  },
+  cookiesPerHour: function() {
+    var customersThisHour = this.randomCustomerPerHour()
+    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
+    cookiesThisHour = Math.floor(cookiesThisHour)
+    //console.log(cookiesThisHour)
+    return cookiesThisHour
+  },
+
+  cookiesPerDay: function() {
+    //calls this cookiesPerHour() 8 times
+    for (var i=0; i<8; i++) {
+      var cookiesThisHour = this.cookiesPerHour()
+      this.cookies[i] = cookiesThisHour;
+      this.totalCookies = this.totalCookies + cookiesThisHour
+      var listItem = document.createElement("li");
+      listItem.innerHTML=this.totalCookies;
+
+      var ul = document.getElementById("PearlDistrictCookies");
+      ul.appendChild(listItem);
+      console.log(this.cookies)
+    }
+    //stores the results of each call in an array
+  }
+}
+
 
 console.log(PioneerSquare)
 PioneerSquare.cookiesPerDay()
@@ -150,3 +240,11 @@ PortlandAirport.addInfo()
 console.log(WashingtonSquare)
 WashingtonSquare.cookiesPerDay()
 WashingtonSquare.addInfo()
+
+console.log(Sellwood)
+Sellwood.cookiesPerDay()
+Sellwood.addInfo()
+
+console.log(PearlDistrict)
+PearlDistrict.cookiesPerDay()
+PearlDistrict.addInfo()
