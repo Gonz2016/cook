3570bd6cd1399b326dc40c85799ecf953857a4ae
Gonzalo <calderongonz@gmail.com>
2016-07-30 14:48:58 -0700

    Added third store....Amazing!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cookie-store
-
+Added a second store information to the cookie project.
--- script.js
+++ script.js
@@ -93,6 +93,51 @@ var PortlandAirport = {
 }
 
 
+var WashingtonSquare = {
+  name: "Washington Square",
+  minimumCustomerPerHour: 11,
+  maximumCustomerPerHour: 38,
+  cookiesPerCustomer: 1.9,
+  cookies: [],
+  totalCookies: 0,
+  addInfo: function() {
+  //setting up a function to get an element from the page by ID
+    var list = document.getElementById('WashingtonSquare')
+    var info = "<h2>"+this.name+"</h2>"
+    list.innerHTML += info;
+  },
+
+  randomCustomerPerHour: function() {
+    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
+    //console.log(customersThisHour)
+    return customersThisHour
+  },
+  cookiesPerHour: function() {
+    var customersThisHour = this.randomCustomerPerHour()
+    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
+    cookiesThisHour = Math.floor(cookiesThisHour)
+    //console.log(cookiesThisHour)
+    return cookiesThisHour
+  },
+
+  cookiesPerDay: function() {
+    //calls this cookiesPerHour() 8 times
+    for (var i=0; i<8; i++) {
+      var cookiesThisHour = this.cookiesPerHour()
+      this.cookies[i] = cookiesThisHour;
+      this.totalCookies = this.totalCookies + cookiesThisHour
+      var listItem = document.createElement("li");
+      listItem.innerHTML=this.totalCookies;
+
+      var ul = document.getElementById("WashingtonCookies");
+      ul.appendChild(listItem);
+      console.log(this.cookies)
+    }
+    //stores the results of each call in an array
+  }
+}
+
+
 
 console.log(PioneerSquare)
 PioneerSquare.cookiesPerDay()
@@ -101,3 +146,7 @@ PioneerSquare.addInfo()
 console.log(PortlandAirport)
 PortlandAirport.cookiesPerDay()
 PortlandAirport.addInfo()
+
+console.log(WashingtonSquare)
+WashingtonSquare.cookiesPerDay()
+WashingtonSquare.addInfo()
