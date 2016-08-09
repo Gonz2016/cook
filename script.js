34a6e5f792d736765d1179168aba75a3e50c34f5
Gonzalo <calderongonz@gmail.com>
2016-08-08 20:25:24 -0700

    Final commit for cookie store project 8

--- script.js
+++ script.js
@@ -1,250 +1,72 @@
+// class # 8 constructor functions
 
+var store = function(location, minCustomer, maxCustomer, avgCookieSale, totalCookies){
+ this.location = location,
+ this.minCustomer = minCustomer,
+ this.maxCustomer = maxCustomer,
+ this.avgCustomer = avgCookieSale,
+ this.totalCookies = totalCookies,
+ this.numberOfCookiesPerHour=[],
 
-
-var PioneerSquare = {
-  name: "Pioneer Square",
-  minimumCustomerPerHour: 17,
-  maximumCustomerPerHour: 88,
-  cookiesPerCustomer: 5.2,
-  cookies: [],
-  totalCookies: 0,
-  addInfo: function() {
-  //setting up a function to get an element from the page by ID
-    var list = document.getElementById('PioneerStand')
-    var info = "<h2>"+this.name+"</h2>"
-    list.innerHTML += info;
-  },
-
-  randomCustomerPerHour: function() {
-    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    //console.log(customersThisHour)
-    return customersThisHour
-  },
-  cookiesPerHour: function() {
-    var customersThisHour = this.randomCustomerPerHour()
-    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
-    cookiesThisHour = Math.floor(cookiesThisHour)
-    //console.log(cookiesThisHour)
-    return cookiesThisHour
-  },
-
-  cookiesPerDay: function() {
-    //calls this cookiesPerHour() 8 times
-    for (var i=0; i<8; i++) {
-      var cookiesThisHour = this.cookiesPerHour()
-      this.cookies[i] = cookiesThisHour;
-      this.totalCookies = this.totalCookies + cookiesThisHour
-      var listItem = document.createElement("li");
-      listItem.innerHTML=this.totalCookies;
-
-      var ul = document.getElementById("PioneerCookies");
-      ul.appendChild(listItem);
-      console.log(this.cookies)
-    }
-    //stores the results of each call in an array
-  }
+this.calculateCookies = function() {
+var total = 0
+for(var i=0; i<8;i++){
+ var number = Math.floor(Math.random()*(this.maxCustomer-this.minCustomer+1)+
+ this.minCustomer);
+ this.numberOfCookiesPerHour.push(number);
+ total += number;
+ this.totalCookies = total;
 }
+return total;
+},
 
-
-
-var PortlandAirport = {
-  name: "Portland Airport",
-  minimumCustomerPerHour: 6,
-  maximumCustomerPerHour: 24,
-  cookiesPerCustomer: 1.2,
-  cookies: [],
-  totalCookies: 0,
-  addInfo: function() {
-  //setting up a function to get an element from the page by ID
-    var list = document.getElementById('AirportStand')
-    var info = "<h2>"+this.name+"</h2>"
-    list.innerHTML += info;
-  },
-
-  randomCustomerPerHour: function() {
-    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    //console.log(customersThisHour)
-    return customersThisHour
-  },
-  cookiesPerHour: function() {
-    var customersThisHour = this.randomCustomerPerHour()
-    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
-    cookiesThisHour = Math.floor(cookiesThisHour)
-    //console.log(cookiesThisHour)
-    return cookiesThisHour
-  },
-
-  cookiesPerDay: function() {
-    //calls this cookiesPerHour() 8 times
-    for (var i=0; i<8; i++) {
-      var cookiesThisHour = this.cookiesPerHour()
-      this.cookies[i] = cookiesThisHour;
-      this.totalCookies = this.totalCookies + cookiesThisHour
-      var listItem = document.createElement("li");
-      listItem.innerHTML=this.totalCookies;
-
-      var ul = document.getElementById("AirportCookies");
-      ul.appendChild(listItem);
-      (this.totalCookies)
-      console.log(this.cookies)
-    }
-    //stores the results of each call in an array
-  }
+this.getCookieOutput = function(){
+  var row = document.createElement('tr');
+  var locationCell = document.createElement('td')
+  var text = document.createTextNode(this.location);
+locationCell.appendChild(text);
+row.appendChild(locationCell);
+  this.calculateCookies();
+var cookieArray = this.numberOfCookiesPerHour;
+for (i=0; i<cookieArray.length; i++){
+  var cell = document.createElement('td');
+  var data = document.createTextNode(cookieArray[i]);
+  cell.appendChild(data);
+  row.appendChild(cell);
 }
-
-
-var WashingtonSquare = {
-  name: "Washington Square",
-  minimumCustomerPerHour: 11,
-  maximumCustomerPerHour: 38,
-  cookiesPerCustomer: 1.9,
-  cookies: [],
-  totalCookies: 0,
-  addInfo: function() {
-  //setting up a function to get an element from the page by ID
-    var list = document.getElementById('WashingtonSquare')
-    var info = "<h2>"+this.name+"</h2>"
-    list.innerHTML += info;
-  },
-
-  randomCustomerPerHour: function() {
-    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    //console.log(customersThisHour)
-    return customersThisHour
-  },
-  cookiesPerHour: function() {
-    var customersThisHour = this.randomCustomerPerHour()
-    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
-    cookiesThisHour = Math.floor(cookiesThisHour)
-    //console.log(cookiesThisHour)
-    return cookiesThisHour
-  },
-
-  cookiesPerDay: function() {
-    //calls this cookiesPerHour() 8 times
-    for (var i=0; i<8; i++) {
-      var cookiesThisHour = this.cookiesPerHour()
-      this.cookies[i] = cookiesThisHour;
-      this.totalCookies = this.totalCookies + cookiesThisHour
-      var listItem = document.createElement("li");
-      listItem.innerHTML=this.totalCookies;
-
-      var ul = document.getElementById("WashingtonCookies");
-      ul.appendChild(listItem);
-      console.log(this.cookies)
-    }
-    //stores the results of each call in an array
-  }
+var data = document.createElement('td');
+var text = document.createTextNode('Total:' + this.totalCookies);
+  data.appendChild(text);
+  row.appendChild(data);
+  return row;
+ }
 }
+var pioneerSquare = new store("Pioneer Square",17,80,5.2,0);
+var portlandAirport = new store("Portland Airport",6, 24,1.2,0);
+var washingtonSquare = new store("Washington Square", 11,38,1.9,0);
+var sellwood = new store("Sellwood",20,48,3.3,0);
+var pearlDistrict = new store("Pearl District", 20,48,3.3,0)
 
-
-var Sellwood = {
-  name: "Sellwood",
-  minimumCustomerPerHour: 20,
-  maximumCustomerPerHour: 48,
-  cookiesPerCustomer: 3.3,
-  cookies: [],
-  totalCookies: 0,
-  addInfo: function() {
-  //setting up a function to get an element from the page by ID
-    var list = document.getElementById('Sellwood')
-    var info = "<h2>"+this.name+"</h2>"
-    list.innerHTML += info;
-  },
-
-  randomCustomerPerHour: function() {
-    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    //console.log(customersThisHour)
-    return customersThisHour
-  },
-  cookiesPerHour: function() {
-    var customersThisHour = this.randomCustomerPerHour()
-    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
-    cookiesThisHour = Math.floor(cookiesThisHour)
-    //console.log(cookiesThisHour)
-    return cookiesThisHour
-  },
-
-  cookiesPerDay: function() {
-    //calls this cookiesPerHour() 8 times
-    for (var i=0; i<8; i++) {
-      var cookiesThisHour = this.cookiesPerHour()
-      this.cookies[i] = cookiesThisHour;
-      this.totalCookies = this.totalCookies + cookiesThisHour
-      var listItem = document.createElement("li");
-      listItem.innerHTML=this.totalCookies;
-
-      var ul = document.getElementById("SellwoodCookies");
-      ul.appendChild(listItem);
-      console.log(this.cookies)
-    }
-    //stores the results of each call in an array
+function createTable(){
+  var time = ['10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
+  var row = document.getElementById('time');
+  var emptyCell = document.createElement('td');
+  row.appendChild(emptyCell);
+  for(i=0; i<time.length;i++){
+  var cell = document.createElement('td');
+  var data = document.createTextNode(time[i]);
+  console.log(data);
+  cell.appendChild(data);
+  row.appendChild(cell);
   }
-}
-
-
-
-var PearlDistrict = {
-  name: "Pearl District",
-  minimumCustomerPerHour: 3,
-  maximumCustomerPerHour: 24,
-  cookiesPerCustomer: 2.6,
-  cookies: [],
-  totalCookies: 0,
-  addInfo: function() {
-  //setting up a function to get an element from the page by ID
-    var list = document.getElementById('PearlDistrict')
-    var info = "<h2>"+this.name+"</h2>"
-    list.innerHTML += info;
-  },
-
-  randomCustomerPerHour: function() {
-    var customersThisHour = Math.floor(Math.random()*(this.maximumCustomerPerHour-this.minimumCustomerPerHour) ) + this.minimumCustomerPerHour
-    //console.log(customersThisHour)
-    return customersThisHour
-  },
-  cookiesPerHour: function() {
-    var customersThisHour = this.randomCustomerPerHour()
-    var cookiesThisHour = this.cookiesPerCustomer * customersThisHour
-    cookiesThisHour = Math.floor(cookiesThisHour)
-    //console.log(cookiesThisHour)
-    return cookiesThisHour
-  },
-
-  cookiesPerDay: function() {
-    //calls this cookiesPerHour() 8 times
-    for (var i=0; i<8; i++) {
-      var cookiesThisHour = this.cookiesPerHour()
-      this.cookies[i] = cookiesThisHour;
-      this.totalCookies = this.totalCookies + cookiesThisHour
-      var listItem = document.createElement("li");
-      listItem.innerHTML=this.totalCookies;
-
-      var ul = document.getElementById("PearlDistrictCookies");
-      ul.appendChild(listItem);
-      console.log(this.cookies)
-    }
-    //stores the results of each call in an array
+  var table = document.getElementById('storeInfo');
+  var data = document.createElement('tr');
+  var cookieSales = [pioneerSquare, portlandAirport, washingtonSquare, sellwood, pearlDistrict];
+  table.appendChild(data);
+  for(var i=0; i<cookieSales.length; i++){
+  var cookieData = cookieSales[i].getCookieOutput();
+  table.appendChild(cookieData);
   }
-}
+ }
 
-
-console.log(PioneerSquare)
-PioneerSquare.cookiesPerDay()
-PioneerSquare.addInfo()
-
-console.log(PortlandAirport)
-PortlandAirport.cookiesPerDay()
-PortlandAirport.addInfo()
-
-console.log(WashingtonSquare)
-WashingtonSquare.cookiesPerDay()
-WashingtonSquare.addInfo()
-
-console.log(Sellwood)
-Sellwood.cookiesPerDay()
-Sellwood.addInfo()
-
-console.log(PearlDistrict)
-PearlDistrict.cookiesPerDay()
-PearlDistrict.addInfo()
+createTable();
